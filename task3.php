b14a7ab0962885ab670a4e97d524d6acc97edae2
hussam524 <hussamhassan352@gmail.com>
2024-06-30 20:53:26 +0300

    husasma

--- task3.php
+++ task3.php
@@ -1,3 +1,118 @@
+<?php
+//juggling and casting
+
+  //  $x = (int)55.168 ;
+ //   echo $x;
+// var_dump(is_int($x));
+
+// float   double
+
+// echo floor((0.7+0.1)*10);
+
+//
+// $name = 'ahmed';
+// echo "$name[0]";
+/*
+$x = "hussam";
+$y = "hassan";
+
+$paragraph =<<<hussam
+this is my new <h1>kabaka</h1>system  to 
+eng $x $y 
+hussam;
+
+
+echo $paragraph;
+*/
+
+// n
+/*
+$database =123 ;
+var_dump($database);
+unset($database);
+var_dump($database);
+*/
+/*
+$x = (bool) null ;
+echo $x;
+var_dump((bool)null);
+*/
+
+
+
+//array
+
+/*
+$array1 = ['hussam' , 'hazem' , 'hamza' , 'hanen'];
+echo $array1[1] ."<br>";
+// echo $array1[20];
+ var_dump(isset($array1[0]));
+
+ print_r($array1);
+ $array1[1]= 'css';
+ $array1[]= 'html';
+ var_dump(count($array1));
+ var_dump($array1);
+*/
+
+//associative array
+/*
+$degrees = [50,100,75,69,85,83];
+$students = ['ahmed' , 'hussam' , 'mohamed', 'khaled' ,'hamza' , 'malek'];
+$degreesofstudents = ['ahmed'=>50 , 'hussam' => 100 , 'mohamed' => 75 , 'khaled' => 69 , 'hamza' => 85 , 'malek' => 83];
+var_dump($degreesofstudents);
+echo $degreesofstudents['ahmed']. "<br>";
+echo $degreesofstudents['hussam'];
+var_dump(count($degreesofstudents));
+*/
+
+// $products = 
+// [
+//     'clothes' => 
+//     [
+//                  ['product name' => 'shirt','price'=> '250',' material' => 'cotton'] , 
+//                  ['trouser','500','jeans'] ,
+//                  ['scarf','150','cotton']
+//     ],
+//     'foods' =>
+//     [
+//         ['tuna','250','cotton'] , 
+//         ['cheese','500','jeans'] ,
+//         ['beef','150','cotton']
+//     ]
+// // ];
+
+// echo $products['foods'][1];
+// var_dump ($products[''][2]);
+// // 
+
+// $arr = [1, 2, 3, 40=>4, 5, 6, 7, 8];
+// // var_dump(array_key_exists(1 , $arr));
+// var_dump(isset($arr[9]));
+// // $r = array_shift($arr);
+
+
+// var_dump([$arr]);
+$x = 2;
+$y = 10;
+
+
+var_dump($x + $y);
+var_dump($x - $y);
+var_dump($x * $y);
+var_dump($x / $y);
+var_dump($x % $y);
+var_dump($x ** $y);
+
+
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +121,5 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-
-    ?>
 </body>
 </html>
