8581d32ecf8e09284411f0ae927a62c7a205572a
hussam524 <hussamhassan352@gmail.com>
2024-08-02 17:17:31 +0300

    delete file

--- task2.php
+++ task2.php
@@ -1,5 +1,116 @@
+<?php
+
+// $is_user_logged_in = true ;
+// $role_of_user = 'admin';
+// /*
+// if($is_user_logged_in == true){
+// echo '<button> log out </button>';
+// }
+// else{
+// echo '<button>log in </button>';
+// }
+// */
+
+// // by using ternary operator
+// $status = $is_user_logged_in == true ? ($role_of_user == 'admin' ? '<button>log out</button><button>dashboard</button>': '<botton>log out</button>') : '<button> log in </button>';
+// echo "$status";
+
+// // // null coalising
+// // $path = '/about';
+// // $p = $path ?? '/';
+// // echo $p ;
+// // // falsy values 
+// // $path = '0';
+// // $p = $path ?: '/';
+// // echo $p ;
+
+// // increment and decrement
+// $x = 50 ;
+// // echo $x++;  //post increment
+// echo $x;
+// echo ++$x;  //pre increment
+// echo $x;
+// echo $x--;  //post decrement
+// echo $x;
+// echo --$x;  //pre deccrement
+// echo $x;
+// $y = null;
+// echo ++$y ; 
+// echo $y;
+// $name = 'hussam';
+// echo $name++;
+// echo $name;
+
+//logical operators 
+
+// $x = true ;
+// $y = false;
+// $z = true;
+// var_dump($x && $y || $z && $y);
+// var_dump($x xor $y);
+// var_dump(  9 | 5); //bitweise operator for or
+// var_dump(  9 & 5); //bitweise operator for and
+// var_dump(  9 ^ 5); //bitweise operator for xor
+// var_dump(9 >> 5) ; // shift right operator 
+// var_dump(9 << 5) ; // shift lefts operator 
+
+// operators in array
+// $z = [0=>'a',1=>'b',2=>'c'];
+// $j = [3=>'d',4=>'e',5=>'h'];
+// var_dump($z + $j);
+// $x =[0=>'a',1=>'b'];
+// $y =[0=>'a',1=>'b'];
+// var_dump($x==$y);
+
+//if conditions
+
+// $student_degree = 95; 
+
+// if($student_degree  >= 90){
+// echo 'student degree is A+';
+// }elseif ($student_degree  <90 && $student_degree >= 80) {
+// echo 'student degree is b+';
+// } elseif ($student_degree  < 80 && $student_degree >= 70) {
+// echo 'student degree is c+';
+// } else{
+// echo 'f';
+// }
+
+// while condition
+
+// $x = 0;
+// while ($x <= 10) {
+//     echo "$x <br>";
+//     $x++;
+// }
+// do while condition
+
+// $x = 0;
+// do {
+//     echo "$x <br>"; 
+//     $x += 5;
+// } while ($x <= 100);
+
+//for condition
+
+// for($i=0; $i <=6; $i++){
+//     echo $i ."<br>" ;
+// }
+//foreach condition
+
+$countries=["eg","sy","us","qt"];
+foreach ($countries as $country) {
+    echo "$country<br>";
+}
 
 
+$countries_with_discount=["eg"=>50,"sy"=>40,"us"=>25,"qt"=>65];
+foreach ($countries_with_discount as $country =>$discount) {
+    echo "the country name is $country and the discount is $discount$<br>";
+}
+?>
+
+ 
 
 
 <!DOCTYPE html>
@@ -11,86 +122,6 @@
 </head>
 <body>
 
-<?php
-// first code print hello world use ( echo and print).
 
-echo  "hello world <br>";
-print "hello world<br>";
-
-// second code make variable and print it with using concatinate
-
-$firstname = "hussam";
-$lastname = "hassan";
-$age = 24 ;
-echo " my name is $firstname". "$lastname <br>";
-echo " my age is $age <br>";
-
-// third code variables .
-
-$x = 2000;
-$y = $x;
-echo "$y <br>";
-
-// fourth code print my name in different ways.
-
-echo $firstname . "<br>";
-echo 'firstname'. "<br>";
-echo "$firstname $lastname" ."<br>" ;
-echo "$firstname " ."<br>";
-echo '$firstname $lastname'."<br>";
-echo "$firstname $lastname"."<br>";
-echo "{$firstname} {$lastname}"."<br>";
-echo '{$firstname} {$lastname}'."<br>";
-
-// constant 
-
-$name;
-$name = "hussam";
-$name = "hazem";
-echo "name<br>";
-
-define ("databasename", "huusam22");
-echo databasename . "<br>";
-
-// make function 
-echo "$firstname .<br>";
-function test (){
-    global $firstname;
-    echo $firstname;
-}
-
-echo PHP_VERSION ."<br>";
-echo PHP_OS_FAMILY."<br>";
-echo PHP_INT_MIN."<br>";
-echo PHP_INT_MAX."<br>";
-
-//boolean values
-
-echo true . "<br>" ;
-echo false . "<br>";
-print true . "<br>" ;
-print false . "<br>";
-
-// if condition
-$test = null ;
-if ($test = true){
-echo "the value is truthy.";
-}else{
-    echo "the value is falsy";
-}
-
-// var_dump and print_r
-
-$isconnected = true;
-$isoffline = false ;
-
-var_dump($isconnected);
-var_dump($isoffline);
-print_r($isconnected);
-print_r($isoffline);
-
-
-
-?> 
 </body>
-</html
+</html>
